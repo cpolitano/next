1a8afef940105ae0d49303dcc3eb4965256518e8
dkim6002 <Danielkim629@gmail.com>
2014-10-02 11:27:25 -0400

    fixed schema

--- db/migrate/20141002145300_create_playlists.rb
+++ db/migrate/20141002145300_create_playlists.rb
@@ -2,8 +2,8 @@ class CreatePlaylists < ActiveRecord::Migration
   def change
     create_table :playlists do |t|
     	t.timestamps
-    	t.references :users
-    	t.references :songs
+    	t.references :user
+    	t.references :song
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20141002145300) do
   create_table "playlists", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "users_id"
-    t.integer  "songs_id"
+    t.integer  "user_id"
+    t.integer  "song_id"
   end
 
   create_table "songs", force: true do |t|
