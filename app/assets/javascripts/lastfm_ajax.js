30df08607d8ef4357a2e2a4b97c5c628569447e3
stbarrientos <stbarrientos@gmail.com>
2014-10-03 15:20:01 -0400

    history displays on playlists#index

--- app/assets/javascripts/lastfm_ajax.js
+++ app/assets/javascripts/lastfm_ajax.js
@@ -2,13 +2,13 @@ var apiKey = "b69b2078d241e58db6e23387b9b495fc";
 
 // get related tracks
 function lastFMRelated(query, artist, limit){
+	$(".history").remove();
 	var relatedTracks = [];
 	$.ajax({
 		url: "http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=" + artist + "&track=" + query + "&api_key=" + apiKey + "&format=json&limit=" + limit,
 		method: "GET",
 		dataType: 'json',
 		success: function(data){
-			console.log(data)
 			for (var i = 0; i < data.similartracks.track.length; i++){
 				spotifyResults(data.similartracks.track[i].name, i);
 			}
@@ -32,15 +32,27 @@ function spotifyResults(query, index){
 	});
 }
 
+function getSearches(){
+	$(".container").append("<div class='history'></div>")
+	$.ajax({
+		url: "/playlists",
+		dataType: "json",
+		method: "GET",
+		success: function(data){
+			for (var i = 0; i < data.length; i++){
+				stringVar = "Artist: " + data[i].artist + ", track: " + data[i].track
+				$(".history").append("<li artist='" + data[i].artist+ "' track='" + data[i].track + "' ><a href='#'>" + stringVar + "</a></li>");
+			}
+		}
+	})
+}
+
 function saveSearch(artistVar, trackVar){
-	$.post({
+	$.ajax({
 		url: '/playlists',
 		method: "POST",
 		dataType: "json",
-		data: { playlist: {artist: artistVar, track: trackVar }},
-		success: function(){
-			console.log("success");
-		}
+		data: { playlist: {artist: artistVar, track: trackVar }}
 	});
 }
 
@@ -58,5 +70,16 @@ $("#search").on('submit', function(event){
 $("a").eq(0).on('click', function(event){
 	event.preventDefault();
 	$(".container").html('<div class="pl-saved"><h2 class="pl-h2">Search for a Track</h2><form id="search"><label for="track">Track</label><input type="search" id="track"><br><label for="artist">Artist</label><input type="search" id="artist"><br><input type="submit" value="Search"></form>');
+});
+
+getSearches();
+
+$(".history li a").on('click', function(event){
+	event.preventDefault();
+	artist = $(this).parent().attr("artist");
+	track = $(this).parent().attr("track");
+	$(".pl-saved").remove();
+	$(".container").html("<div class='pl-players'></div>");
+	lastFMRelated(track, artist, 10);
 });
 
